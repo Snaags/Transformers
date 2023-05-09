6bf74e973a86b021a67a3d3d89407de0f260b342
jackwilkie <jackwilkie78987@gmail.com>
2023-05-09 17:33:37 +0100

    updated tokenisers

--- bpe_malnet_implementation.py
+++ bpe_malnet_implementation.py
@@ -10,15 +10,19 @@ created: 28/04/2023
 import numpy as np
 from PIL import Image
 import os
+import random 
 
 #preprocessing malnet
 class malnet_dataloader():
 
-    def __init__(self, dataset_path = '~/data/malnet.cc.gatech.edu/image-data/full-data-as-6GB/malnet-images/'):
+    def __init__(self, dataset_path =
+                 '~/data/malnet.cc.gatech.edu/image-data/full-data-as-6GB/malnet-images/',
+                seed = time.time()):
         
         #store dataset path
         self.dataset_path = os.path.expanduser(path)
-
+        
+        self.rng = random.Random(seed)
 
     #function to convert malware image to string of bytes
     def malnet_image_to_bytes(self, file_path):
@@ -32,6 +36,36 @@ class malnet_dataloader():
         #convert to numpy array and flatter
         return np.array(img_array).flatten()
     
+    def load_byte_file(self, file_path):
+        
+
+
+        return byte_string
+    
+    def random_sample(self, seed, dir_path: str, dir_size = 0):
+        
+        #initalise uniform sampling params (if dir size is known)
+        sampling_probability = 0
+        if dir_size > 0: sampling_probability = 1/dir_size
+
+        #initalise reservoir sampling params
+        num_samples = 0
+        selected_sample = None
+
+        #iterate over directory
+        for root, dirs, files in os.walk(dir_path):
+            for file in files:
+                
+                # use uniform sampling if size known (rng.random() > 0)
+                if rng.random() < sampling_probability: return os.path.join(root,file)  # return sample with uniform probability
+
+                #sample file with proability 1/n
+                if rng.randrange(num_files = 0): 
+                    selected_sample = os.path.join(root, file)
+                
+                num_samples += 1 #increment sample counter 
+        
+       return selected_sample 
 
     #iterate over entire dataest one file at a time
     def iterate_datset(self):
