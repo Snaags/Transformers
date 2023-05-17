e46a74f73c9ea6e9124067bf32c1e612990d9147
jackwilkie <jackwilkie78987@gmail.com>
2023-05-17 16:27:54 +0100

    add gru autoencoder code

--- lstm_tokeniser.py
+++ lstm_tokeniser.py
@@ -7,129 +7,154 @@ Created on: 08/05/23
 """
 
 import torch as T
+import torch.nn.functional as F
 import numpy as np
 
-class ByteLevelAutoencoder(T.nn.Module):
-    def __init__(self, encoder_layers, decoder_layers, input_dim = 256, hidden_dim = 512, enocder_bidirectional = True, decoder_bidirectional = True, architecture_type = 'LSTM'):
 
-        super(BiLSTMAutoencoder, self).__init__() #initalise torch nn modlue
+#Encoder using bidirectional GRU Architecture                                                                                                                                            
+class BiGRUEncoder(T.nn.Module):                                                                                                                                                           
+    
+    def __init__(self, input_dim = embedding_dim, hidden_dim = 512, num_layers = 1, dropout = 0.0, bidirectional = True, device = 'cuda'):                                                         
+                                                                                                                                                                                          
+        super(BiGRUEncoder, self).__init__() #initalise T.nn.Module                                                                                                                                 
+
+        #store model configuration                                                                                                                                                       
+        self.input_dim = input_dim                                                                                                                                                       
+        self.hidden_dim = hidden_dim                                                                                                                                                     
+        self.num_layers = num_layers                                                                                                                                                     
+        self.dropout = dropout                                                                                                                                                           
+        self.bidirectional = bidirectional                                                                                                                                               
+        self.device = device                                                                                                                                                                      
+
+        #initalise gru                                                                                                                                                                   
+        self.gru = T.nn.GRU(input_dim, hidden_dim, num_layers, batch_first = True, bidirectional = bidirectional, dropout = dropout, device = device)                                    
+
+    def forward(self, x):                                                                                                                                                                   
+
+        # get final time step hidden state from last layer gru, discard intermediate oututs                                                                                              
+        _, hidden = self.gru(x)                                                                                                                                                          
+
+        # concantenate hidden states from each direction                                                                                                                                 
+        hidden = T.cat((hidden[-2], hidden[-1]), dim = -1)                                                                                                                                              
+
+        return hidden  # return dim is B, D * hidden_dim, where D = 2 if bidirectional, otherwise 1                                                                                           
+
+
+# Decoder using birdirectional GRU Architecture                                                                                                                                           
+class BiGRUDecoder(T.nn.Module):                                                                                                                                                         
+
+    def __init__(self, input_dim = 512, hidden_dim = 512, vocab_size = 258, embedding_layer = None, num_layers = 1, dropout = 0.0, bidirectional = False, init_scheme = 'zeros', device = 'cuda'):                                       
+
+        super(BiGRUDecoder, self).__init__() #initalise T.nn.module                                                                                                                      
+
+        #store model configuration                                                                                                                                                       
+        self.input_dim = input_dim                                                                                                                                                       
+        self.hidden_dim = hidden_dim                                                                                                                                                     
+        self.num_layers = num_layers                                                                                                                                                     
+        self.dropout = dropout                                                                                                                                                           
+        self.bidirectional = bidirectional                                                                                                                                               
+        self.device = device                                                                                                                                                             
+        self.vocab_size = vocab_size
         
-        #define recurrent block architecture for encoding/decoding
-        if architecture_type.lower() = 'lstm':
-            self.reccurent_block = T.nn.LSTM 
-        elif architecture_type.lower() == 'gru':
-            self.recurrent_block = T.nn.GRU
-
+        if self.bidirectional: 
+            self.D = 2
         else:
-            raise ValueError('ERROR::: Invalid Architecture Type')
+            self.D = 1
         
+        #initalise gru                                                                                                                                                                   
+        self.gru = T.nn.GRU(input_dim, hidden_dim, num_layers,batch_first = True, bidirectional = bidirectional, dropout = dropout)                                                               
+        self.embedding_layer = embedding_layer
 
-        #define encoder and decoder architectures:
-        self.encoder = recurrent_block(input_dim, hidden_dim, num_layers = encoder_layers, batch_first  = True, bidirectional = encoder_bidirectional)
-        self.decoder = recurrent_block(2 * hidden_dim, input_dim, num_layers = decoder_layers, batch_first = True, bidirectional = decoder_bidirectional)
+        #initalise fully connected layer for final classifcation                                                                                                                         
+        if bidirectional:                                                                                                                                                                
+            fc_size = hidden_dim *2                                                                                                                                                      
+        else:                                                                                                                                                                            
+            fc_size = hidden_dim                                                                                                                                                         
 
-
-    #function to encode data
-    def encode(self, x):
-        pass
+        self.fc = T.nn.Linear(fc_size, self.vocab_size)                                                                                                                                       
+        
+        self.embedding_layer  = embedding_layer
+        
+        # if init scheme is zeros, first gru layer is initalied with encoder output as hidden state whilst rest are initalised with zero
+        # if init scheme is encoder, all gru layers are initilased with encoder output as hidden state
+        
+        self.init_scheme = init_scheme.lower()
+        
+        if self.init_scheme != 'zeros' and self.init_scheme != 'encoder': 
+            raise ValueError('BiGRUDecoder Invalid init_scheme selected, valid options are: "zeros", "encoder"')
+            
     
+    def forward(self, token_in, hidden, argmax = False):                                                                                                                                                 
 
-    #function to decode encoded data 
-    def decode(self, x):
-        pass
+        '''                                                                                                                                                                              
+        hidden is initial hidden state from encoder (encoded token)                                                                                                                      
 
-
-    #implement autoencode forward pass
-    def forward(self, x):
+        '''                                                                                                                                                                              
         
-        #pass data throu
-        _, (hidden, _) = self.encode(x)
-
-        hidden = hidden.view(1, x.size(0), -1)
-
-        output, _ = self.decoder(hidden.repeat(x.size(1), 1, 1)).transpose(0,1)
-        return output
-
-
-#Encoder using bidirectional GRU Architecture
-class BiGRUEncoder(T.nn.Module):
-    
-    def __init__(self, input_dim = 256, hidden_dim = 512, num_layers = 1, dropout = 0.0, bidirectional = True, device = 'cuda'):
-        
-        super(BiGRUEncoder, self).__init__()  #initalise T.nn.Module
-        
-        #store model configuration
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.num_layers = num_layers
-        self.dropout = dropout
-        self.bidirectional = bidirectional
-        self.device = device
-
-        #initalise gru
-        self.gru = T.nn.GRU(input_dim, hidden_dim, num_layers, batch_first = True, bidirectional = bidirectional, dropout = dropout, device = device)
-
-    def forward(self, x):
-         
-        # get final time step hidden state from last layer gru, discard intermediate oututs
-        _, hidden = self.gru(x)        
-        
-        # concantenate hidden states from each direction
-        hidden = T.cat((hidden[-2]. hidden[-1]), dim = -1)
-
-        return hidden  # return dim is B, D * hidden_dim, where D = 2 if bidirectional, otherwise 1 
-
-
-#Decoder using birdirectional GRU Architecture
-class BiGRUDecoder(T.nn.Module):
-
-    def __init__(self, output_dim: int, input_dim = 512, hidden_dim = 512, num_layers = 1, dropout = 0.0, bidirectional = False, device = 'cuda'):
-        
-        super(BiGRUDecoder, self).__init__() #initalise T.nn.module
-        
-        #store model configuration
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.num_layers = num_layers
-        self.dropout = dropout
-        self.bidirectional = bidirectional 
-        self.device = device
-
-        #initalise gru
-        self.gru = T.nn.GRU(input_dim, hidden_dim, num_layers, batch_first = True, bidirectional = bidirectional, dropout)
-
-
-        #initalise fully connected layer for final classifcation
+        _, hidden = self.gru(token_in, hidden)
+            
         if bidirectional:
-            fc_size = hidden_dim *2
+            out = T.cat((hidden[-2], hidden[-1]), dim = -1)  
+                
         else:
-            fc_size = hidden_dim
-
-        self.fc = T.nn.Linear(fc_size, output_dim)
-
-
-    def forward(self, h, seq_len = 512):
-        '''
-        hidden is initial hidden state from encoder (encoded token) 
+            out = hidden[-1]
+            
+            
+        #get predicted tokens
+        pred_token = F.softmax(self.fc(out))
         
-        '''
+        if argmax: _, pred_token = T.max(pred_token, dim =-1) 
         
-        # inital state
-        batch_size = h0.size(0) #batch is first dim since batch_first = True
+        return hidden, pred_tokem 
+      
+    
+    def decode_seq(self, h0 = None, seq_len = 512):
+        
         outputs = []
-
-        # generate inital input to gru (zero matrix) of dims (B, seq_len, Hin) Note sequence length is one as single token used as input
-        x = T.zeros(batch_size, 1, self.fc.out_features, device = self.device)
         
-        for _ in range(seq_len):
+        #get inital hidden state
+        hidden = self.encoder_token_to_hiddne_state(h0)
+        
+        # get inital input as start token (256)
+        start_token = self.tokenise_int(T.tensor(256, device = self.device, dtype = T.int64))
+        next_input = self.tokenise_int(start_token)
+        
+        #convert to (N, h) dim 
+        next_input = next_input.view((1,-1))
+        
+        # iterate over sequence
+        for i in range(seq_len):
             
-            x, hidden = self.gru(x, hidden) #get output and hidden state
+            hidden, pred_token = self(next_token, hidden, argmax = True)
             
-            x = self.fc(x)
-            outputs.append(x)
+            output.append(pred_token)
             
-
-
+            next_input = self.tokenise_int(pred_token)
+            
+        return outputs
+        
+        
+    def tokenise_int(self, x: int):
+        
+        if self.embedding_layer is not None: 
+            return self.embedding_layer(pred_token)
+        
+        else:
+            return F.one_hot(pred_token, self.vocab_size)
+            
+    def encoder_token_to_hidden_state(self, h):
+        
+        if self.init_scheme == 'zeros':
+            h0 = T.zeros((self.D * self.num_layers, h.size(0), self.hidden_dim),dtype = T.float32, device = device)
+            h0[0] = h
+            
+        elif self.init_scheme == 'encoder':
+            h0 = h.unsqueeze(0).repeat(self.D*self.num_layers, 1, 1)
+        
+        return h0
+        
+        
+        
 
 
 #define autoencoder using budirectional GRUs
--- parse_androzoo.py
+++ parse_androzoo.py
@@ -8,122 +8,13 @@ created on 06/05/23
 '''
 
 import os
-import shutil
-import gzip
-import binascii
-from androguard.core.bytecodes import apk
-import requests
 import multiprocessing
 from tqdm import tqdm
 import sys
 import argparse
 import json
 import traceback
-
-#function to downlaod apk from androzoo using sha256 has and api key
-def download_apk(apk_hash: str, download_path: str, api_key: str):
-    
-    # define apk download url and get
-    url = f" https://androzoo.uni.lu/api/download?apikey={api_key}&sha256={apk_hash}"
-    response = requests.get(url, stream=True)
-
-    if response.status_code == 200:
-        #if file found download to specified path
-        with open(download_path, 'wb') as apk_file: 
-            for chunk in response.iter_content(chunk_size=8192):
-                apk_file.write(chunk)
-        
-
-    # if file not been downloaded correctly
-    else:
-        print(f"Error downloading APK. Status code: {response.status_code}")
-                     
-
-#function to extract raw bytecode from apk file
-def extract_bytecode(apk_path: str):
-
-    # Load the APK file
-    a = apk.APK(apk_path)
-
-    # Get the DEX files
-    dex_files = a.get_all_dex()
-
-    # Convert the generator to a list and return the raw bytecode
-    return list(dex_files)
-                     
-
-#function to convert binary formatted data to string of hexadecimal representation
-def binary_to_hex(binary_data, use_format = 'byte_string'):
-        #hex_bytes = binascii.hexlify(binary_data)
-        hex_bytes = binary_data
-
-        if use_format == 'text': 
-            return binascii.hexlify(hex_bytes).decode('utf-8')
-        elif use_format == 'byte_string':
-            return hex_bytes
-        else:
-            raise ValueError('ERROR::: Invalid Format For Parsing Binary')
-
-def write_large_string_to_file(file_path, large_string, chunk_size=1024 * 1024):
-    with open(file_path, 'w', encoding='utf-8') as file:
-        start_index = 0
-        end_index = chunk_size
-        
-        while start_index < len(large_string):
-            file.write(large_string[start_index:end_index])
-            start_index += chunk_size
-            end_index += chunk_size
-
-
-#function to save hex string as .txt file 
-def save_hex_strings_to_file(hex_string, file_path, save_format = 'byte_string'):
-     
-    #save as byte string
-    if save_format == 'byte_string':
-        with open(f'{file_path}.bin', 'wb') as file:
-            file.write(hex_string)
-
-    #compress using gzip
-    elif save_format == 'compressed':
-        with gzip.open(f'{file_path}.txt', "wt") as file:
-            file.write(hex_string)
-    
-    #save as raw txt file
-    elif save_format == 'text':
-        write_large_string_to_file(f'{file_path}.txt', hex_string)
-        
-        '''
-        with open(file_path, "w") as file:
-            file.write(hex_string)
-        '''
-    else:
-        raise ValueError('ERROR::: Invalid hex save format specified')
-
-#function to downlaod and return hex string from sha256 hash                
-def download_hex(apk_hash: str, api_key: str, use_format = 'byte_string', temp_path = os.path.expanduser('~/temp/temp_apk.apk')):
-    
-    #download android apk from androzoo using previously defined function
-    download_apk(apk_hash, temp_path, api_key)
-    dex_files = extract_bytecode(temp_path)  #find dex files in apk
-
-    #convert dex files to hex dumps
-    hex_files = [binary_to_hex(x, use_format = use_format) for x in dex_files]
-    
-    #combine in case of multiple dex files
-    if use_format == 'byte_string':
-        hex_string = b''.join(hex_files)
-    else:
-        hex_string = ''.join(hex_files)
-    os.remove(temp_path)
-                                                                
-    return hex_string
-                                                                    
-
-#generator function for downloading hex files
-def generate_hex_files(apk_hashes, api_key, temp_path = os.path.expanduser('~/temp_apks/temp_apk')):
-    for apk_hash in apk_hashes:        
-        yield download_hex(apk_hash,api_key, temp_path)
-
+from datasets.androzoo import download_hex, save_hex_strings_to_file
 
 #worker for downloading dataset
 def download_worker(args):
